d6bd30fd270c7453485ca9d7b17703110faf0fa5
David <davidteng95@gmail.com>
2023-05-16 05:44:15 +0000

    added iterating over an array with while

--- packing-list.js
+++ packing-list.js
@@ -9,7 +9,16 @@ const packingList = [
   "cooking utensils",
 ];
 
+//using for
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+//using while
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
